b67c3b89afb5424047a978f8ddcad741eb9510db
jonathanhumphrey <jlh473@txstate.edu>
2020-12-23 10:33:13 -0600

    Added conditional syling to the IQ score

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import ModalContainer from './Modal'
 import './App.css';
 
 function App() {
+  // creates a slice of state in order to create a toggle button to toggle the state 
   const [show, setShow] = useState(false);
-  const [iqScore, setIqScore] = useState(110);
-  const [showScore, setShowScore] = useState(false);
-  
+  //toggles the visibility of the modal
   const toggleShow = () => {
     setShow(!show)
   }
-  const testOver = () => {
-    setShow(!show)
-    setShowScore(true)
-  }
+
+  // creates a slice of state in order to set the value of the starting IQ value
+  const [iqScore, setIqScore] = useState(110);
+  // sets the iqScore based on this very complex alorithm
   const iqCalculator = () => {
     setIqScore(iqScore => iqScore - 1)
     console.log(iqScore)
   }
 
+  // creates a slice of state in order to show the score
+  const [showScore, setShowScore] = useState(false);
+  
+  // Activates when the close button within the Modal is clicked
+  // setShow is set to true as well as setShowScore in order to close out of the modal and display the user's score
+  const testOver = () => {
+    setShow(!show)
+    setShowScore(true)
+  }
+  
+  //creates a timer that fires the function iqCalculator every .500 (500 milli) seconds
   useEffect(() => {
     if (show) {
       const timer = setTimeout(() => {           
@@ -27,6 +37,23 @@ function App() {
       return () => clearTimeout(timer);
     }
   });
+
+  const styles = {
+    fontSize: 40,
+    backgroundColor: 'grey'
+  }
+  if (iqScore > 100) {
+    styles.color =  '#09f530'
+  }
+  else if (iqScore < 99) {
+    styles.color = 'yellow'
+  }
+  else if (iqScore < 85) {
+    styles.color = 'orange'
+  }
+  else if (iqScore < 70) {
+    styles.color = 'red'
+  }
 
   return (
     <div className="App">
@@ -44,10 +71,9 @@ function App() {
             testOver={testOver}
           />
           {!show && showScore ?
-            <div class="score">
-              <p>Your Score is: {iqScore}</p>
-            </div>
-            : ""}
+            <div className="score">
+              <p style={styles}>Your IQ is: {iqScore}</p>
+            </div> : ""}
         </div>
       </div>
     </div>
--- src/Modal.js
+++ src/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Modal from 'react-bootstrap/Modal'
-
+//passes in the slices of state to allow the button to change the state
 function ModalContainer({show, toggleShow, testOver}) {
     return (
         <div>
